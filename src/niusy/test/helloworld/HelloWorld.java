239286dc748f0147131a95a48793d1f0b4dc267f
Sean <904178767@qq.com>
2015-03-17 21:29:15 +0800

    Update HelloWorld.java

--- src/niusy/test/helloworld/HelloWorld.java
+++ src/niusy/test/helloworld/HelloWorld.java
@@ -12,8 +12,8 @@ public class HelloWorld {
 
 	public void getNumberOfList(){
 		list.add(1);
-		attribute.add("attributeF");
-		attribute.add("attributeF");
+		attribute.add("attributeG");
+		attribute.add("attributeG");
 		list.add(2);
 		attribute.add("attriB");
 		list.add(3);
